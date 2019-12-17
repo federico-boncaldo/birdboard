50f3606fa6f00c3cae92461e446883ef3ac4e857
Federico Boncaldo <federico@cryptolab.net>
2019-12-17 12:29:02 +0000

    Use projects factory for project validation tests

--- tests/Feature/ProjectsTest.php
+++ tests/Feature/ProjectsTest.php
@@ -30,12 +30,14 @@ class ProjectsTest extends TestCase
     /** @test */
     public function a_project_requires_a_title()
     {
-        $this->post('/projects', [])->assertSessionHasErrors('title');
+        $attributes = factory('App\Project')->raw(['title' => '']);
+        $this->post('/projects', $attributes)->assertSessionHasErrors('title');
     }
 
     /** @test */
     public function a_project_requires_a_description()
     {
-        $this->post('/projects', [])->assertSessionHasErrors('description');
+        $attributes = factory('App\Project')->raw(['description' => '']);
+        $this->post('/projects', $attributes)->assertSessionHasErrors('description');
     }
 }
