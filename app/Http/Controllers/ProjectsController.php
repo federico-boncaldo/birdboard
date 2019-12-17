66e5fd62a7173583719b6db56438d17cda5cc1bc
Federico Boncaldo <federico@cryptolab.net>
2019-12-17 12:54:50 +0000

    Add show method to projects controller

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -27,4 +27,9 @@ class ProjectsController extends Controller
 
 	    return redirect('/projects');
 	}
+
+	public function show(Project $project)
+	{
+		return view('projects.show', compact('project'));
+	}
 }
