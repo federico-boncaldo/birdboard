e594fc6ebfebee81e6c8c5f407db890c360fc0ca
Federico Boncaldo <federico@cryptolab.net>
2019-12-17 11:19:29 +0000

    Create ProjectTest class
    The class consists of two methods: one tests if a user can create a
    project the other that the project requires a title

---
+++ app/Http/Controllers/ProjectsController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Project;
+
+class ProjectsController extends Controller
+{
+	public function index()
+	{
+		$projects = Project::all();
+
+		return view('projects.index', compact('projects'));
+	}
+
+	public function store()
+	{
+	    $attributes = request()->validate(
+	        [
+	            'title' => 'required',
+	            'description' => 'required'
+	        ]
+	    );
+
+	    Project::create($attributes);
+
+	    return redirect('/projects');
+	}
+}
