10ab0faa0204674934a0cc4b4d62a0747d6eba18
Federico Boncaldo <federico@cryptolab.net>
2020-01-09 13:04:17 +0000

    Add custom colours for blue and blue-light

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,17 @@
 module.exports = {
   theme: {
-    extend: {},
-    boxShadow: {
-    	default: '0 0 5px 0 rgba(0, 0, 0, 0.08)'
+    extend: {
+	    boxShadow: {
+	    	default: '0 0 5px 0 rgba(0, 0, 0, 0.08)'
+	    },
+	    colors: {
+	    	'grey': 'rgba(0, 0, 0, 0.4)',
+	    	'grey-light': '#F5F6F9',
+	    	'white': '#FFFFFF',
+	    	'blue' : '#47cdff',
+	    	'blue-light' : '#8ae2f2',
+	    }
     },
-    colors: {
-    	'grey': 'rgba(0, 0, 0, 0.4)',
-    	'grey-light': '#F5F6F9',
-    	'white': '#FFFFFF'
-    }
   },
   variants: {},
   plugins: []
