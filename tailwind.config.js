7c6936edcd2be7f9ce5842995f572c3e2eaa9636
Federico Boncaldo <federico@cryptolab.net>
2020-01-09 10:49:26 +0000

    Create tailwind config file

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  theme: {
+    extend: {},
+    boxShadow: {
+    	default: '0 0 5px 0 rgba(0, 0, 0, 0.08)'
+    },
+    colors: {
+    	'grey': 'rgba(0, 0, 0, 0.4)',
+    	'grey-light': '#F5F6F9',
+    	'white': '#FFFFFF'
+    }
+  },
+  variants: {},
+  plugins: []
+}
