82a082d802c0e705d3b5412d6d53a4159a6382cf
Federico Boncaldo <federico@cryptolab.net>
2020-01-09 13:02:09 +0000

    Adjust header and boxes in header - projects index

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -1,20 +1,28 @@
 @extends('layouts.app')
 
 @section('content')
-	<div class="flex items-center mb-3">
-		<a href="/projects/create">New Project</a>
-	</div>
+	<header class="flex items-center mb-3 py-4">
+		<div class="flex justify-between items-center w-full">
+			<h2 class="text-grey text-sm font-normal">My Projects</h2>
+
+			<a href="/projects/create" class="button">New Project</a>
+		</div>
+	</header>
 
 
-	<div class="flex">
+	<main class="lg:flex lg:flex-wrap -mx-3">
 		@forelse ($projects as $project)
-			<div class="bg-white mr-4 rounded shadow w-1/4 p-5">
-				<h3 class="font-normal text-xl mb-6 py-4">{{ $project->title }}</h3>
-				<div class="text-grey">{{ Str::limit($project->description) }}</div>
+			<div class="lg:w-1/3 px-3 pb-6">
+				<div class="bg-white mr-4 p-5 rounded-lg shadow">
+					<h3 class="font-normal text-xl mb-6 py-4 -ml-5 border-l-4 border-blue-light pl-4 mb-3">
+						<a href="{{ $project->path() }}">{{ $project->title }}</a>
+					</h3>
+					<div class="text-grey">{{ Str::limit($project->description) }}</div>
+				</div>
 			</div>
 		@empty
 			<div>No projects yet.</div>
 		@endforelse
-	</div>
+	</main>
 
 @endsection
