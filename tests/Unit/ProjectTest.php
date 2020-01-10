7e8f6c5e79983471c230ae9509beeefca1b33e61
Federico Boncaldo <federico@cryptolab.net>
2020-01-10 16:12:09 +0000

    Add it_can_add_a_task test to Project Test

--- tests/Unit/ProjectTest.php
+++ tests/Unit/ProjectTest.php
@@ -30,4 +30,15 @@ class ProjectTest extends TestCase
 
 		$this->assertInstanceOf(User::class, $project->owner);
 	}
+
+	/** @test */
+	public function it_can_add_a_task()
+	{
+		$project = factory('App\Project')->create();
+
+		$task = $project->addTask('Test task');
+
+		$this->assertCount(1, $project->tasks);
+		$this->assertTrue($project->tasks->contains($task));
+	}
 }
