96dcd968b7faa66d941165c075ee1f8148a58d9f
Federico Boncaldo <federico@cryptolab.net>
2020-01-15 17:33:15 +0000

    Use task path method in show view

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -16,7 +16,7 @@
 				<h2 class="text-lg text-grey mb-3 text-sm font-normal text-lg">Tasks</h2>
 				@foreach ($project->tasks as $task)
 					<div class="card mb-3">
-						<form method="POST" action="{{ $project->path() . '/tasks/' . $task->id }}">
+						<form method="POST" action="{{ $task->path() }}">
 							@method('PATCH')
 							@csrf
 							<div class="flex">
