d0fc8ea023508168b4c341705a92991df8bd4861
Federico Boncaldo <federico@cryptolab.net>
2020-01-15 17:26:17 +0000

    Add unit test class for task model

---
+++ tests/Unit/TaskTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Unit;
+
+// use PHPUnit\Framework\TestCase;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Task;
+use App\Project;
+
+class TaskTest extends TestCase
+{
+	use RefreshDatabase;
+
+	/** @test */
+	public function it_belongs_to_a_project()
+	{
+		$task = factory(Task::class)->create();
+
+		$this->assertInstanceOf(Project::class, $task->project);
+	}
+
+	/** @test */
+	public function it_has_a_path()
+	{
+		$task = factory(Task::class)->create();
+
+		$this->assertEquals('/projects/' . $task->project->id . '/tasks/' . $task->id, $task->path());
+	}
+}
