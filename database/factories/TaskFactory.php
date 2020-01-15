39ff7efac1b7e52da54a3473ef16a5db8f8f363e
Federico Boncaldo <federico@cryptolab.net>
2020-01-15 17:28:05 +0000

    Add project_id to Task Factory

--- database/factories/TaskFactory.php
+++ database/factories/TaskFactory.php
@@ -3,10 +3,13 @@
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
 use App\Task;
+use App\Project;
 use Faker\Generator as Faker;
 
 $factory->define(Task::class, function (Faker $faker) {
     return [
         'body' => $faker->paragraph,
-    ];
+        //it's not necessary to create a closure to get the project_id
+        'project_id' => factory(Project::class)
+     ];
 });
