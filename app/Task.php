d33d7f617755ba0c456e6ff0cc09af2312ec473c
Federico Boncaldo <federico@cryptolab.net>
2020-01-15 17:26:56 +0000

    Add path() method to Task model

--- app/Task.php
+++ app/Task.php
@@ -15,4 +15,9 @@ class Task extends Model
     {
     	return $this->belongsTo(Project::class);
     }
+
+    public function path()
+    {
+    	return '/projects/' . $this->project->id . '/tasks/' . $this->id;
+    }
 }
