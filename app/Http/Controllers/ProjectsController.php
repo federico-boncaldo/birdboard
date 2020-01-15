9bf09aca1af0a4dfcefe018ed3909afa528b3ab7
Federico Boncaldo <federico@cryptolab.net>
2020-01-15 13:42:23 +0000

    Redirect to project page when creating new project

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -28,9 +28,9 @@ class ProjectsController extends Controller
             ]
         );
 
-        auth()->user()->projects()->create($attributes);
+        $project = auth()->user()->projects()->create($attributes);
 
-        return redirect('/projects');
+        return redirect($project->path());
     }
 
     public function show(Project $project)
