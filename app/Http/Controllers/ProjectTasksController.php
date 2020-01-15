329f938eda4336c1042bf9c93638ee8cee321211
Federico Boncaldo <federico@cryptolab.net>
2020-01-15 16:24:41 +0000

    Add update method

--- app/Http/Controllers/ProjectTasksController.php
+++ app/Http/Controllers/ProjectTasksController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Project;
+use App\Task;
 
 class ProjectTasksController extends Controller
 {
@@ -16,6 +17,16 @@ class ProjectTasksController extends Controller
     	request()->validate(['body' => 'required']);
 
     	$project->addTask(request('body'));
+
+    	return redirect($project->path());
+    }
+
+    public function update(Project $project, Task $task)
+    {
+    	$task->update([
+    		'body' => request('body'),
+    		'completed' => request()->has('completed')
+    	]);
 
     	return redirect($project->path());
     }
