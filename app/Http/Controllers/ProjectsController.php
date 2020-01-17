f1490de276b6f66e063ae956caaaccdd5728b160
Federico Boncaldo <federico@cryptolab.net>
2020-01-17 14:00:19 +0000

    Use authorize method to check the project owner

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -36,10 +36,7 @@ class ProjectsController extends Controller
 
     public function update(Project $project)
     {
-        if(auth()->user()->isNot($project->owner))
-        {
-            abort(403);
-        }
+        $this->authorize('update', $project);
 
         $project->update(request(['notes']));
 
@@ -48,9 +45,7 @@ class ProjectsController extends Controller
 
     public function show(Project $project)
     {
-    	if(auth()->user()->isNot($project->owner)){
-    		abort(403);
-    	}
+        $this->authorize('update', $project);
 
         return view('projects.show', compact('project'));
     }
