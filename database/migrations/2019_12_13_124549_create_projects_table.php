2f1b6a1901d44dc1234965be5d06a7afb5402e5d
Federico Boncaldo <federico@cryptolab.net>
2020-01-17 13:23:36 +0000

    Add notes column to projects table migration

--- database/migrations/2019_12_13_124549_create_projects_table.php
+++ database/migrations/2019_12_13_124549_create_projects_table.php
@@ -18,6 +18,7 @@ class CreateProjectsTable extends Migration
             $table->unsignedBigInteger('owner_id');
             $table->string('title')->nullable();
             $table->text('description')->nullable();
+            $table->text('notes')->nullable();
             $table->timestamps();
 
             $table->foreign('owner_id')->references('id')->on('users')->onDelete('cascade');
