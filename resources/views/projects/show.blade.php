ea87805cd19cb4a637854792f5a9b10307b6eaeb
Federico Boncaldo <federico@cryptolab.net>
2020-01-17 13:27:21 +0000

    Add form to update project notes

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -39,7 +39,20 @@
 
 			<div>
 				<h2 class="text-lg text-grey mb-3 text-sm font-normal">General Notes</h2>
-				<textarea class="card w-full" style="min-height: 200px">Lorem ipsum.</textarea>
+				{{-- general notes --}}
+				<form method="POST" action="{{ $project->path() }}">
+					@csrf
+					@method('PATCH')
+
+					<textarea
+						name="notes"
+						class="card w-full mb-4"
+						style="min-height: 200px"
+						placeholder="Anything special that you want to make a note of?"
+					>{{ $project->notes }}</textarea>
+
+					<button type="submit" class="button">Save</button>
+				</form>
 			</div>
 		</div>
 
