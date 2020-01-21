ce909e8f87473441050884d9556794291ac2faf8
Federico Boncaldo <federico@cryptolab.net>
2020-01-21 14:21:03 +0000

    Create UpdateProjectRequest class

---
+++ app/Http/Requests/UpdateProjectRequest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Gate;
+
+class UpdateProjectRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return Gate::allows('update', $this->project());
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'title' => 'sometimes|required',
+            'description' => 'sometimes|required',
+            'notes' => 'nullable'
+        ];
+    }
+
+    public function project()
+    {
+        return $this->route('project');
+    }
+
+    public function persist()
+    {
+        $this->project()->update($this->validated());
+    }
+}
