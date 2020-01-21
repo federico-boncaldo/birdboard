95a8aff9277d924d9ce8f48d6dc4ab55bb531bc8
Federico Boncaldo <federico@cryptolab.net>
2020-01-21 15:43:31 +0000

    Create Activity model

---
+++ app/Activity.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Activity extends Model
+{
+    protected $fillable = [
+    	'project_id',
+    	'description'
+    ];
+}
