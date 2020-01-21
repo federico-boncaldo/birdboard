87420f023d2a0dbd5a3519ffe3c6d457d8f64839
Federico Boncaldo <federico@cryptolab.net>
2020-01-21 13:28:06 +0000

    Add edit method to projects controller

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -28,6 +28,11 @@ class ProjectsController extends Controller
         return redirect($project->path());
     }
 
+    public function edit(Project $project)
+    {
+        return view('projects.edit', compact('project'));
+    }
+
     public function update(Project $project)
     {
         $this->authorize('update', $project);
