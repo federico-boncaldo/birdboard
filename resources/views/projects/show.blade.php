e1998da5afe1e79b516a92d1813d6e2d42f8b946
Federico Boncaldo <federico@cryptolab.net>
2020-01-21 13:39:50 +0000

    Add errors visualisation to project page

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -53,6 +53,14 @@
 
 					<button type="submit" class="button">Save</button>
 				</form>
+
+				@if($errors->any())
+				<div class="field mt-6">
+					@foreach($errors->all() as $error)
+						<li class="text-sm text-red">{{ $error }}</li>
+					@endforeach
+				</div>
+				@endif
 			</div>
 		</div>
 
