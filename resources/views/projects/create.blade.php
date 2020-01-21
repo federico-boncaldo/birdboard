8265555f6b566a725e9df9eb6fffa1c35294fec5
Federico Boncaldo <federico@cryptolab.net>
2020-01-21 13:31:32 +0000

    Refactor project create view by using form template

--- resources/views/projects/create.blade.php
+++ resources/views/projects/create.blade.php
@@ -1,26 +1,14 @@
 @extends('layouts.app')
 
 @section('content')
-	<h1>Create a New Project</h1>
-	<form method="POST" action="/projects">
-		@csrf
-
-		<div class="field">
-			<label class="label" for="title">Project Title</label>
-			<div>
-				<input type="text" name="title" placeholder="Project Title" value="{{ old('title') }}">
-			</div>
-		</div>
-
-		<div class="field">
-			<label class="label" for="title">Project Description</label>
-			<div>
-				<textarea name="description" placeholder="Project Description" >{{ old('description') }}</textarea>
-			</div>
-		</div>
-
-		<button type="submit">Create Project</button>
-		<a href="/projects">Cancel</a>
-
-	</form>
+	<div class="lg:w-1/2 lg:mx-auto bg-white p-6 md:py-12 md:px-16 rounded shadow">
+		<h1>Create a New Project</h1>
+		<form method="POST" action="/projects">
+			@csrf
+			@include('projects.form',
+			[
+				'project' => new App\Project,
+				'buttonText' => 'Create Project'
+			])
+	</div>
 @endsection
