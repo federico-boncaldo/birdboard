c4df74274ab2eb58e5a7ebb97ed9adcc224f775b
Federico Boncaldo <federico@cryptolab.net>
2020-01-22 12:58:19 +0000

    Add completed field to task factory

--- database/factories/TaskFactory.php
+++ database/factories/TaskFactory.php
@@ -9,6 +9,7 @@ use Faker\Generator as Faker;
 $factory->define(Task::class, function (Faker $faker) {
     return [
         'body' => $faker->paragraph,
+        'completed' => false,
         //it's not necessary to create a closure to get the project_id
         'project_id' => factory(Project::class)
      ];
