8cef99a3f29d02cdb58d838d802357bb29e5cb95
Federico Boncaldo <federico@cryptolab.net>
2020-01-22 14:47:07 +0000

    Rename RecordActivityTest to TriggerActivityTest

--- tests/Feature/TriggerActivityTest.php
+++ tests/Feature/TriggerActivityTest.php
@@ -7,7 +7,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
-class RecordActivityTest extends TestCase
+class TriggerActivityTest extends TestCase
 {
     use RefreshDatabase;
 
