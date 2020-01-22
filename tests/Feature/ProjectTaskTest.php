f96f7941a366a0cc05f07e49adb86288c4e8e6e9
Federico Boncaldo <federico@cryptolab.net>
2020-01-22 14:03:31 +0000

    Add a_task_can_be_marked_as_incomplete test

--- tests/Feature/ProjectTaskTest.php
+++ tests/Feature/ProjectTaskTest.php
@@ -82,12 +82,57 @@ class ProjectTaskTest extends TestCase
 
         $this->patch($project->tasks->first()->path(), [
             'body' => 'changed',
+        ]);
+
+        $this->assertDatabaseHas('tasks', [
+            'body' => 'changed',
+        ]);
+
+    }
+
+    /** @test */
+    public function a_task_can_be_completed()
+    {
+        $project = app(ProjectFactory::class)
+            ->ownedBy($this->signIn())
+            ->withTask(1)
+            ->create();
+
+        $this->patch($project->tasks->first()->path(), [
+            'body' => 'changed',
             'completed' => true
         ]);
 
         $this->assertDatabaseHas('tasks', [
             'body' => 'changed',
             'completed' => true
+        ]);
+
+    }
+
+    /** @test */
+    public function a_task_can_be_marked_as_incomplete()
+    {
+        $this->withoutExceptionHandling();
+
+        $project = app(ProjectFactory::class)
+            ->ownedBy($this->signIn())
+            ->withTask(1)
+            ->create();
+
+        $this->patch($project->tasks->first()->path(), [
+            'body' => 'changed',
+            'completed' => true
+        ]);
+
+        $this->patch($project->tasks->first()->path(), [
+            'body' => 'changed',
+            'completed' => false
+        ]);
+
+        $this->assertDatabaseHas('tasks', [
+            'body' => 'changed',
+            'completed' => false
         ]);
 
     }
