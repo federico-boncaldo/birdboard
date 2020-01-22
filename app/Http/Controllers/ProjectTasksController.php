c5cadc0f726dcf5d1b564153a92de3230718ce16
Federico Boncaldo <federico@cryptolab.net>
2020-01-22 14:07:30 +0000

    Add task->incomplete() to update method

--- app/Http/Controllers/ProjectTasksController.php
+++ app/Http/Controllers/ProjectTasksController.php
@@ -27,8 +27,10 @@ class ProjectTasksController extends Controller
 
         $task->update(['body' => request('body')]);
 
-        if(request()->has('completed')) {
+        if(request('completed')) {
             $task->complete();
+        }else{
+            $task->incomplete();
         }
 
     	return redirect($project->path());
