8f8a12616d2457ed2403f8c88070d0a457d1b268
Federico Boncaldo <federico@cryptolab.net>
2020-01-22 12:55:17 +0000

    Refactor by calling recordActivity from project object

--- app/Observers/ProjectObserver.php
+++ app/Observers/ProjectObserver.php
@@ -15,7 +15,7 @@ class ProjectObserver
      */
     public function created(Project $project)
     {
-        $this->recordActivity($project, 'created');
+        $project->recordActivity('created');
     }
 
     /**
@@ -26,14 +26,7 @@ class ProjectObserver
      */
     public function updated(Project $project)
     {
-        $this->recordActivity($project, 'updated');
+        $project->recordActivity('updated');
     }
 
-    protected function recordActivity($project, $type)
-    {
-        Activity::create([
-            'project_id' => $project->id,
-            'description' => $type
-        ]);
-    }
 }
