348fe12419ca3cd5f58866e7e0c1843b3f17db58
Federico Boncaldo <federico@cryptolab.net>
2020-01-22 14:06:23 +0000

    Add it_can_be_marked_as_incomplete test

--- tests/Unit/TaskTest.php
+++ tests/Unit/TaskTest.php
@@ -41,4 +41,17 @@ class TaskTest extends TestCase
 
 		$this->assertTrue($task->fresh()->completed);
 	}
+
+	/** @test */
+	public function it_can_be_marked_as_incomplete()
+	{
+		$task = factory(Task::class)->create(['completed' => true]);
+
+		$this->assertTrue($task->completed);
+
+		$task->incomplete();
+
+		$this->assertFalse($task->fresh()->completed);
+
+	}
 }
