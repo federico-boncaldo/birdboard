84f79ea5bf36d8150456efdcd08155ca8dad7c0a
Federico Boncaldo <federico@cryptolab.net>
2020-01-22 12:47:33 +0000

    Add test to record an activity while updating tasks

--- tests/Feature/ActivityFeedTest.php
+++ tests/Feature/ActivityFeedTest.php
@@ -17,7 +17,7 @@ class ActivityFeedTest extends TestCase
         $project = ProjectFactory::create();
 
         $this->assertCount(1, $project->activity);
-        $this->assertEquals('created', $project->activity[0]->description);
+        $this->assertEquals('created', $project->activity->last()->description);
     }
 
     /** @test */
@@ -28,7 +28,37 @@ class ActivityFeedTest extends TestCase
         $project->update(['title' => 'Changed']);
 
         $this->assertCount(2, $project->activity);
-        $this->assertEquals('updated', $project->activity[1]->description);
+        $this->assertEquals('updated', $project->activity->last()->description);
+
+    }
+
+    /** @test */
+    public function creating_a_new_task_records_project_activity()
+    {
+        $project = ProjectFactory::create();
+
+        $project->addTask('Some Task');
+
+        $this->assertCount(2, $project->activity);
+        $this->assertEquals('created_task', $project->activity->last()->description);
+
+    }
+
+    /** @test */
+    public function completing_a_task_records_project_activity()
+    {
+        $project = ProjectFactory::
+            ownedBy($this->signIn())
+            ->withTask(1)
+            ->create();
+
+        $this->patch($project->tasks[0]->path(), [
+            'body' => 'foobar',
+            'completed' => true
+        ]);
+
+        $this->assertCount(3, $project->activity);
+        $this->assertEquals('completed_task', $project->activity->last()->description);
 
     }
 }
