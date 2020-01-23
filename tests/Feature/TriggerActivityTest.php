b9b8f60030dda93f58ed2d2d2fbf11a5498b3f96
Federico Boncaldo <federico@cryptolab.net>
2020-01-23 16:51:12 +0000

    Update updating_a_project by checking before and after status

--- tests/Feature/TriggerActivityTest.php
+++ tests/Feature/TriggerActivityTest.php
@@ -25,11 +25,23 @@ class TriggerActivityTest extends TestCase
     public function updating_a_project()
     {
         $project = ProjectFactory::create();
+        $originalTitle = $project->title;
 
         $project->update(['title' => 'Changed']);
 
         $this->assertCount(2, $project->activity);
-        $this->assertEquals('updated', $project->activity->last()->description);
+
+        tap($project->activity->last(), function($activity) use ($originalTitle) {
+            $this->assertEquals('updated', $activity->description);
+
+            $expected = [
+                'before' => ['title' => $originalTitle],
+                'after' => ['title' => 'Changed']
+            ];
+
+            $this->assertEquals($expected, $activity->changes);
+        });
+
 
     }
 
