60fd7bd2fc39105b9b2f234b1b0f35a996362af9
Federico Boncaldo <federico@cryptolab.net>
2020-01-27 12:04:59 +0000

    Add form to delete project in card view

--- resources/views/projects/card.blade.php
+++ resources/views/projects/card.blade.php
@@ -2,5 +2,13 @@
 	<h3 class="font-normal text-xl mb-6 py-4 -ml-5 border-l-4 border-blue-light pl-4 mb-3">
 		<a href="{{ $project->path() }}">{{ $project->title }}</a>
 	</h3>
-	<div class="text-grey">{{ Str::limit($project->description) }}</div>
+	<div class="text-grey mb-4">{{ Str::limit($project->description) }}</div>
+
+	<footer>
+		<form method="POST" action="{{ $project->path() }}" class="text-right">
+			@method('DELETE')
+			@csrf
+			<button type="submit" class="text-xs">Delete</button>
+		</form>
+	</footer>
 </div>
