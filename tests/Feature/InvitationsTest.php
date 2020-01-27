13c8542616772e8b0209fb92313a42bc65f40f1a
Federico Boncaldo <federico@cryptolab.net>
2020-01-27 15:20:18 +0000

    Create InvitationsTest
    The class cointains a_project_can_invite_a_user test

---
+++ tests/Feature/InvitationsTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use Facades\Tests\Setup\ProjectFactory;
+use App\User;
+
+class InvitationsTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function a_project_can_invite_a_user()
+    {
+        $this->withoutExceptionHandling();
+
+        $project = ProjectFactory::create();
+
+        $project->invite($newUser = factory(User::class)->create());
+
+        $this->signIn($newUser);
+        $this->post(action('ProjectTasksController@store', $project), $task = ['body' => 'Foo task']);
+
+        $this->assertDatabaseHas('tasks', $task);
+    }
+}
