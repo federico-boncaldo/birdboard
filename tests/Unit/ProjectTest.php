466bfad258d450ae0598f873903f9a5ca1212ff9
Federico Boncaldo <federico@cryptolab.net>
2020-01-27 14:55:57 +0000

    Add it_can_invite_a_user test

--- tests/Unit/ProjectTest.php
+++ tests/Unit/ProjectTest.php
@@ -41,4 +41,14 @@ class ProjectTest extends TestCase
 		$this->assertCount(1, $project->tasks);
 		$this->assertTrue($project->tasks->contains($task));
 	}
+
+	/** @test */
+	public function it_can_invite_a_user()
+	{
+		$project = factory(Project::class)->create();
+
+		$project->invite($user = factory(User::class)->create());
+
+		$this->assertTrue($project->members->contains($user));
+	}
 }
