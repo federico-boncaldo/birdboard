2d950e95293bd1bd33f6e6f384e7af696410e4bc
Federico Boncaldo <federico@cryptolab.net>
2020-01-27 11:52:18 +0000

    Add project delete route

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,7 @@ Route::group(['middleware' => 'auth'], function(){
 	Route::get('/projects/{project}/edit', 'ProjectsController@edit');
 	Route::patch('/projects/{project}', 'ProjectsController@update');
 	Route::get('/projects/{project}', 'ProjectsController@show');
+	Route::delete('/projects/{project}', 'ProjectsController@destroy');
 
 	Route::post('/projects/{project}/tasks', 'ProjectTasksController@store');
 	Route::patch('/projects/{project}/tasks/{task}', 'ProjectTasksController@update');
