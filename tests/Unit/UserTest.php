70371e320776bde732c141c18697683cd872258d
Federico Boncaldo <federico@cryptolab.net>
2020-01-27 16:51:02 +0000

    Add a_user_has_accessible_projects test

--- tests/Unit/UserTest.php
+++ tests/Unit/UserTest.php
@@ -8,6 +8,7 @@ use Tests\TestCase;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use App\User;
+use Facades\Tests\Setup\ProjectFactory;
 
 class UserTest extends TestCase
 {
@@ -19,5 +20,27 @@ class UserTest extends TestCase
 		$user = factory(User::class)->create();
 
 		$this->assertInstanceOf(Collection::class, $user->projects);
+	}
+
+	/** @test */
+	public function a_user_has_accessible_projects()
+	{
+		$john = $this->signIn();
+
+		$project = ProjectFactory::ownedBy($john)->create();
+
+		$this->assertCount(1, $john->accessibleProjects());
+
+		$sally = factory(User::class)->create();
+		$nick = factory(User::class)->create();
+
+		$sallyProject = ProjectFactory::ownedBy($sally)->create();
+		$sallyProject->invite($nick);
+
+		$this->assertCount(1, $john->accessibleProjects());
+
+		$sallyProject->invite($john);
+
+		$this->assertCount(2, $john->accessibleProjects());
 	}
 }
