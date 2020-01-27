ce4d8c35825894174b7c6f45b3ca8c48eaaa1277
Federico Boncaldo <federico@cryptolab.net>
2020-01-27 12:22:35 +0000

    Add authorize method to destroy method
    It checks if the user can delete the project

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -61,6 +61,8 @@ class ProjectsController extends Controller
 
     public function destroy(Project $project)
     {
+        $this->authorize('update', $project);
+
         $project->delete();
 
         return redirect('projects');
