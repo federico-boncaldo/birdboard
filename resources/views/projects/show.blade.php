32222911e275187bb08a63dc597dcbb4ed1f4455
Federico Boncaldo <federico@cryptolab.net>
2020-01-28 13:15:15 +0000

    Add project members to show view
    A gravatar is associated to every user

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -1,12 +1,24 @@
 @extends('layouts.app')
 
 @section('content')
-	<header class="flex items-center mb-3 py-4">
+	<header class="flex items-center mb-3 pb-4">
 		<div class="flex justify-between items-end w-full">
 			<p class="text-grey text-sm font-normal">
-				<a href="/projects" class="text-grey text-sm font-normal">My Projects</a> / {{ $project->title }}</p>
+				<a href="/projects" class="text-grey text-sm font-normal no-underline hover:underline">My Projects</a> / {{ $project->title }}</p>
 
-			<a href="{{ $project->path() . '/edit' }}" class="button">Edit Project</a>
+			<div class="flex items-center">
+				@foreach($project->members as $member)
+					<img
+						src="{{ gravatar_url($member->email) }}"
+						alt="{{ $member->name }}'s avatar"
+						class="rounded-full w-8 mr-2">
+				@endforeach
+				<img
+					src="{{ gravatar_url($project->owner->email) }}"
+					alt="{{ $project->owner->name }}'s avatar"
+					class="rounded-full w-8 mr-2">
+				<a href="{{ $project->path() . '/edit' }}" class="button ml-4">Edit Project</a>
+			</div>
 		</div>
 	</header>
 
