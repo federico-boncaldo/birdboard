2ef599b3d4f59a2bf80ed70c91d8460dd5f1908d
Federico Boncaldo <federico@cryptolab.net>
2020-01-28 13:19:37 +0000

    Update front end assets

--- public/js/app.js
+++ public/js/app.js
@@ -49613,8 +49613,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/federico/programming/laravel/birdboard/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /home/federico/programming/laravel/birdboard/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /home/vagrant/code/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /home/vagrant/code/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
