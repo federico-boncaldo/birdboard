afb98f7a8f9959e4776bcf90ae97c4e2ef31e400
Federico Boncaldo <federico@cryptolab.net>
2020-01-28 14:51:40 +0000

    Add validation rule and message - user email address

--- app/Http/Controllers/ProjectInvitationsController.php
+++ app/Http/Controllers/ProjectInvitationsController.php
@@ -10,6 +10,12 @@ class ProjectInvitationsController extends Controller
 {
     public function store(Project $project)
     {
+    	request()->validate([
+    		'email' => 'exists:users,email'
+    	], [
+    		'email.exists' => 'The user you are inviting must have a Birdboard account.'
+    	]);
+
     	$user = User::whereEmail(request('email'))->first();
 
     	$project->invite($user);
