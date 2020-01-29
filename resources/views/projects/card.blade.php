44adf9acb93bd92f34948c22d88f33f55cb347af
Federico Boncaldo <federico@cryptolab.net>
2020-01-29 16:50:47 +0000

    Adjust card view with display flex classes

--- resources/views/projects/card.blade.php
+++ resources/views/projects/card.blade.php
@@ -1,8 +1,8 @@
-<div class="card">
+<div class="card flex flex-col">
 	<h3 class="font-normal text-xl mb-6 py-4 -ml-5 border-l-4 border-blue-light pl-4 mb-3">
 		<a href="{{ $project->path() }}">{{ $project->title }}</a>
 	</h3>
-	<div class="text-grey mb-4">{{ Str::limit($project->description) }}</div>
+	<div class="text-grey mb-4 flex-1">{{ Str::limit($project->description) }}</div>
 
 	<footer>
 		<form method="POST" action="{{ $project->path() }}" class="text-right">
