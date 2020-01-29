c7a9396dbd110c7b0335d89090a2786a6110d354
Federico Boncaldo <federico@cryptolab.net>
2020-01-29 16:37:03 +0000

    Adjust test to make sure the invited user cannot invite

--- tests/Feature/InvitationsTest.php
+++ tests/Feature/InvitationsTest.php
@@ -15,9 +15,21 @@ class InvitationsTest extends TestCase
     /** @test */
     public function non_owners_may_not_invite_users()
     {
-        $this->actingAs(factory(User::class)->create())
-            ->post(ProjectFactory::create()->path() . '/invitations')
-            ->assertStatus(403);
+        $project = ProjectFactory::create();
+
+        $user = factory(User::class)->create();
+
+        $assertInvitationForbidden = function() use($user, $project) {
+            $this->actingAs($user)
+                ->post($project->path() . '/invitations')
+                ->assertStatus(403);
+        };
+
+        $assertInvitationForbidden();
+
+        $project->invite($user);
+
+        $assertInvitationForbidden();
     }
 
     /** @test */
@@ -46,7 +58,7 @@ class InvitationsTest extends TestCase
         ])
         ->assertSessionHasErrors([
             'email' => 'The user you are inviting must have a Birdboard account.'
-        ]);
+        ], null, 'invitations');
     }
 
     /** @test */
