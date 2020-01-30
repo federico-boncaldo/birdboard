d3cca32205c96bb4a96d18ead37d8c3fd6914e84
Federico Boncaldo <federico@cryptolab.net>
2020-01-30 12:09:20 +0000

    Refactor a_user_can_create_a_project
    It uses the project factory class to generate the new project attributes

--- tests/Feature/ManageProjectsTest.php
+++ tests/Feature/ManageProjectsTest.php
@@ -35,11 +35,7 @@ class ManageProjectsTest extends TestCase
 
         $this->get('/projects/create')->assertStatus(200);
 
-        $attributes = [
-            'title' => $this->faker->sentence,
-            'description' => $this->faker->paragraph,
-            'notes' => 'General notes here.'
-        ];
+        $attributes = factory(Project::class)->raw(['owner_id' => auth()->id()]);
 
         $response = $this->post('/projects', $attributes);
 
