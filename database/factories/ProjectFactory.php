497821891b8079619c3f0d653b2769a486c6c716
Federico Boncaldo <federico@cryptolab.net>
2020-01-30 12:11:25 +0000

    Add notes field in Project Factory

--- database/factories/ProjectFactory.php
+++ database/factories/ProjectFactory.php
@@ -9,6 +9,7 @@ $factory->define(Project::class, function (Faker $faker) {
     return [
         'title' => $faker->sentence(4),
         'description' => $faker->sentence(4),
+        'notes' => $faker->sentence(4),
         'owner_id' => function() {
         	return factory(App\User::class)->create()->id;
         }
