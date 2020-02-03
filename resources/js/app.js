e4724a4f5fbfd8951a2e5022db007a3fdf31797b
Federico Boncaldo <federico@cryptolab.net>
2020-02-03 17:54:45 +0000

    Register vue dropdown component in js file

--- resources/js/app.js
+++ resources/js/app.js
@@ -14,6 +14,7 @@ Vue.use(VModal);
 
 Vue.component('theme-switcher', require('./components/ThemeSwitcher.vue').default);
 Vue.component('new-project-modal', require('./components/NewProjectModal.vue').default);
+Vue.component('dropdown', require('./components/Dropdown.vue').default);
 
 const app = new Vue({
     el: '#app',
