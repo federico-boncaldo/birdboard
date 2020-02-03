7e5aa2a47f4003bec5be46a072d042acf4663d1b
Federico Boncaldo <federico@cryptolab.net>
2020-02-03 13:47:30 +0000

    Use body property instead of value for tasks

--- resources/js/components/NewProjectModal.vue
+++ resources/js/components/NewProjectModal.vue
@@ -36,11 +36,11 @@
 							class="border border-muted-light p-2 mb-2 text-xs block w-full rounded"
 							placeholder="Task 1"
 							v-for="task in form.tasks"
-							v-model="task.value">
+							v-model="task.body">
 					</div>
 
 
-	                <button class="inline-flex items-center text-xs" @click="addTask">
+	                <button type="button" class="inline-flex items-center text-xs" @click="addTask">
 	                    <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18" class="mr-2">
 	                        <g fill="none" fill-rule="evenodd" opacity=".307">
 	                            <path stroke="#000" stroke-opacity=".012" stroke-width="0" d="M-3-3h24v24H-3z"></path>
@@ -54,7 +54,7 @@
 		</form>
 
 		<footer class="flex justify-end">
-			<button class="button mr-4 is-outlined" @click="$modal.hide('new-project')">Cancel</button>
+			<button type="button" class="button mr-4 is-outlined" @click="$modal.hide('new-project')">Cancel</button>
 			<button class="button" @click="submit">Create Project</button>
 		</footer>
 	</modal>
@@ -68,7 +68,7 @@
 					title : "",
 					description: "",
 					tasks : [
-						{ value: ""}
+						{ body: ""}
 					]
 				},
 
