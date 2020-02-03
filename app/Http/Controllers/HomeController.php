9428b36ea5c019b7a8ed23d7350a7cabe83733cf
Federico Boncaldo <federico@cryptolab.net>
2020-02-03 17:57:14 +0000

    Redirect from home to projects index page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,13 +6,9 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Contracts\Support\Renderable
-     */
+
     public function index()
     {
-        return view('home');
+        return redirect('projects');
     }
 }
