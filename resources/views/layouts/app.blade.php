dfce949ed82cb081d9a59a687be5f667f68f8a42
Federico Boncaldo <federico@cryptolab.net>
2020-03-03 16:55:34 +0000

    Adjust visualisation of main page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -41,36 +41,37 @@
 
                     <div>
                         <!-- Right Side Of Navbar -->
-                        <div class="flex items-center navbar-nav ml-auto list-reset">
+                        <div class="flex items-center ml-auto">
                             <!-- Authentication Links -->
                             @guest
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                <a class="text-accent mr-4 no-underline hover:underline" href="{{ route('login') }}">{{ __('Login') }}</a>
+
                                 @if (Route::has('register'))
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                    <a class="text-accent no-underline hover:underline" href="{{ route('register') }}">{{ __('Register') }}</a>
                                 @endif
                             @else
                                 <theme-switcher></theme-switcher>
+
                                 <dropdown align="right" width="200px">
                                     <template v-slot:trigger>
                                         <button
                                             class="flex items-center text-default no-underline text-sm focus:outline-none"
+                                            v-pre
                                         >
                                             <img width="35"
                                                  class="rounded-full mr-3"
                                                  src="{{ gravatar_url(auth()->user()->email) }}">
 
-                                            {{ Auth::user()->name }}
+                                            {{ auth()->user()->name }}
                                         </button>
                                     </template>
 
                                     <form id="logout-form" method="POST" action="/logout">
                                         @csrf
+
                                         <button type="submit" class="dropdown-menu-link w-full text-left">Logout</button>
                                     </form>
-
                                 </dropdown>
-
-
                             @endguest
                         </div>
                     </div>
